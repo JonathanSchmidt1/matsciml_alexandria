629a02fb54cc80fd08156664b8a13615fd5bd98f
JonathanSchmidt1 <schmidt.jonathan138@googlemail.com>
2024-01-11 23:07:32 +0100

    add __init__ imports back in

--- matsciml/datasets/alexandria/__init__.py
+++ matsciml/datasets/alexandria/__init__.py
@@ -2,4 +2,6 @@ from __future__ import annotations
 
 from pathlib import Path
 
+from matsciml.datasets.alexandria.dataset import AlexandriaDataset, M3GAlexandriaDataset
+
 alexandria_devset = Path(__file__).parents[0].joinpath("devset")
